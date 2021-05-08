26b446dcffcf452c44ff4c045e705b5cc6c9dca2
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-08 13:10:51 -0600

    schema created

--- models/post.js
+++ models/post.js
@@ -1,2 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+//The post schema to be saved in the DB
+const postSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    content: { type: String, required: true },
+    creator: { type: Object, required: true },
+  },
+  { timestamps: true } //Construct a timeStamp automatically 
+);
+
+module.exports = mongoose.model('POST', postSchema)
