dc35e7415646cfae0cd71a1584722645824ed329
filipecamargos <filipe.atrabalho@gmail.com>
2021-06-07 20:12:12 -0600

    User sign up validation added

--- app.js
+++ app.js
@@ -64,8 +64,9 @@ app.use("/auth", authRoutes);
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
+  const data = error.data;
   const message = error.message;
-  res.status(status).json({ message: message });
+  res.status(status).json({ message: message, data: data });
 });
 
 //Set the connection
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,29 @@ const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 
+/***************************************
+ * PUT -> SIGN UP FOR THE SOCIAL MEDIA
+ ***************************************/
 exports.signup = (req, res, next) => {
+  //check for errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    customErrorStatus("Validation Failed", 422, errors.array());
+  }
+
   //get all the req's data
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
+
+};
+
+/******************************************
+ * All the Helping Functions will go below
+ *****************************************/
+const customErrorStatus = (message, statusCode, errorData = null) => {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  error.data = errorData;
+  throw error;
 };
